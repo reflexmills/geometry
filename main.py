4afb22e60bb94da43d07610f781cc61fc83a3223
reflexmills <lodog5378@gmail.com>
2025-05-25 18:34:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -236,6 +236,6 @@ async def profile(message: types.Message):
         reply_markup=main_keyboard()
     )
 
-if name == 'main':
+if __name__ == '__main__':
     import asyncio
     asyncio.run(dp.start_polling(bot))
